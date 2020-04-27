3d5dd93c09e0fabfc32966870e1a189116cba9ef
Siarhei Piashko <peshko.sergey@gmail.com>
2020-04-28 00:26:58 +0300

    add server

---
+++ server/src/main/java/com/example/fortune/impl/FortunesResource.java
@@ -0,0 +1,34 @@
+package com.example.fortune.impl;
+
+import com.linkedin.restli.server.annotations.RestLiCollection;
+import com.linkedin.restli.server.resources.CollectionResourceTemplate;
+import com.example.fortune.Fortune;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Simple Rest.li Resource that serves up a fortune cookie.
+ */
+@RestLiCollection(name = "fortunes", namespace = "com.example.fortune")
+public class FortunesResource extends CollectionResourceTemplate<Long, Fortune> {
+
+    // In-memory store for the fortunes
+    static Map<Long, String> fortunes = new HashMap<Long, String>();
+    static {
+        fortunes.put(1L, "Today is your lucky day.");
+        fortunes.put(2L, "There's no time like the present.");
+        fortunes.put(3L, "Don't worry, be happy.");
+    }
+
+    @Override
+    public Fortune get(Long key) {
+        // Retrieve the requested fortune
+        String fortune = fortunes.get(key);
+        if (fortune == null) {
+            fortune = "Your luck has run out. No fortune for id = " + key;
+        }
+
+        // return an object that represents the fortune cookie
+        return new Fortune().setFortune(fortune);
+    }
+}
